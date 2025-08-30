b53092ba5d4b355833775301358ea73f338d644c
Mohamed Amine Rouissi <95948593+Am1n0N@users.noreply.github.com>
2025-08-30 01:18:08 +0100

    clerk aut bug fix

--- datarag - app/middleware.ts
+++ datarag - app/middleware.ts
@@ -1,7 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"], 
+  publicRoutes: [
+    "/",
+    "/%22/sign-in%22"
+  ],
+
 });
 
 export const config = {
